c998544f3f0f6587d4117805a5d3a3268ea07dff
z-chubinidze <120126922+z-chubinidze@users.noreply.github.com>
2023-05-31 11:04:24 +0400

    Add files via upload

---
+++ js-14.js
@@ -0,0 +1,14 @@
+
+
+function shecvla() {
+
+    var circle = document.getElementById("blue-circle");
+    
+    circle.style.marginLeft = "1016px";
+  
+    circle.style.backgroundColor = "green";
+
+    circle.style.transition = "4s";
+   
+  
+  }
